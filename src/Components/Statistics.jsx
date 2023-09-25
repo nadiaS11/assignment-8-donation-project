cf19eebaf97fac664260a386fb24ce5a824745a8
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-09-25 18:22:02 +0600

    pie chart added in Statistics component

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -1,8 +1,27 @@
-import React from "react";
-import PropTypes from "prop-types";
+import { Chart } from "react-google-charts";
+const Statistics = () => {
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Available donations", 12],
+    ["Your donations", 2],
+  ];
 
-const Statistics = (props) => {
-  return <div>statistics here</div>;
+  const options = {
+    title: "Your donations Stats",
+  };
+
+  return (
+    <div>
+      <Chart
+        className="text-xl"
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"600px"}
+      />
+    </div>
+  );
 };
 
 Statistics.propTypes = {};
